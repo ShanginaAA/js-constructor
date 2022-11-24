ad713e5d0ca2eee5deba1920ed99af5315daa22e
Alexshan20 <shangina.1998@gmail.com>
2022-11-24 15:03:48 +0300

    f

---
+++ src/model.js
@@ -0,0 +1,51 @@
+import image from './assets/image.png';
+import { Block } from './classes/blocks';
+
+const text = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, laboriosam error tempore ab veniam et minima dolorem vitae? Voluptates deserunt impedit rerum debitis iusto explicabo?`;
+
+export const model = [
+  new Block('title', 'Конструктор сайтов на чистом JavaScripts', {
+    tag: 'h2',
+    styles: {
+      background: 'linear-gradient(to right, #ff0099, #493240)',
+      color: '#fff',
+      padding: '1.5rem',
+      'text-align': 'center',
+    },
+  }),
+  new Block('image', image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center',
+    },
+    imageStyles: {
+      width: '500px',
+      height: 'auto',
+    },
+    alt: 'Картинка',
+  }),
+  new Block(
+    'columns',
+    [
+      'Приложение на чистом JavaScript, без использования библиотек',
+      'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
+      'JavaScript - это просто, интересно. Научись создавать любые UI своими руками',
+    ],
+    {
+      styles: {
+        background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+        padding: '2rem',
+        color: '#fff',
+        'font-weight': 'bold',
+      },
+    },
+  ),
+  new Block('text', text, {
+    styles: {
+      background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+      padding: '1rem',
+      'font-weight': 'bold',
+    },
+  }),
+];
